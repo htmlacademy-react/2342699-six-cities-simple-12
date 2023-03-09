261c7ef3e89a646af93714f1d2c9bcc88c143784
Daria Romanova <d.e.romanova@mail.ru>
2023-03-09 13:54:00 +0300

    3.13 Добавлен роутер

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,26 @@
 import Main from '../../pages/main/Main';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from '../../pages/login/Login';
+import Room from '../../pages/room/Room';
+import Page404 from '../../pages/page404/Page404';
 
 type AppProps = {
-    offersQuantity: number;
+  offersQuantity: number;
 }
 
-function App({offersQuantity}: AppProps): JSX.Element {
-  return <Main offersQuantity={offersQuantity}/>;
+function App({ offersQuantity }: AppProps): JSX.Element {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/'>
+        <Route index element={<Main offersQuantity={offersQuantity} />} />
+        <Route path='login' element={<Login/>} />
+        <Route path='offer/:id' element={<Room/>} />
+        <Route path='*' element={<Page404/>} />
+      </Route>
+    </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
